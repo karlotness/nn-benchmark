e09151083c8a979b4b53ff536acda8a348fe4c42
Karl Otness <karl.otness@nyu.edu>
2020-09-22 10:27:56 -0400

    Add base definition for systems.

---
+++ src/systems/defs.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+
+TrajectoryResult = namedtuple("TrajectoryResult", ["q", "p", "dq_dt", "dp_dt", "t_steps"])
+
+class System:
+    def __init__(self):
+        pass
+
+    def hamiltonian(self, coord):
+        """Compute hamiltonian for each coord. Input [batch, 2, n_grid] or [2, n_grid]. Return [batch] or scalar"""
+        raise NotImplementedError("Subclass this")
+
+    def derivative(self, coord):
+        """Return local q, p time derivative for each of coord. Input shape [batch, 2, n_grid] or [2, n_grid]"""
+        raise NotImplementedError("Subclass this")
+
+    def generate_trajectory(self, x0, t_span, time_step_size, **kwargs):
+        """x0 is initial state, t_span = (t_min, t_max), time_step_size with
+        t_span determines # steps
+        """
+        raise NotImplementedError("Subclass this")
