fc66e956305741234153ed91260f666d2703665f
Karl Otness <karl.otness@nyu.edu>
2020-09-22 10:28:21 -0400

    Import basic HNN implementation.

---
+++ src/methods/hnn.py
@@ -0,0 +1,55 @@
+import torch
+from collections import namedtuple
+
+HnnTimeDerivativeFields = namedtuple("HnnTimeDerivativeFields", ["conservative", "solenoidal"])
+
+
+def permutation_tensor(self, n):
+    mat = torch.eye(n)
+    return torch.cat((mat[n//2:], -mat[:n//2]))
+
+
+class HNN(torch.nn.Module):
+    def __init__(self, input_dim, base_model, field_type="solenoidal"):
+        super().__init__()
+        self.base_model = base_model
+        self._permute_mat = permutation_tensor(input_dim)
+        self.field_type = field_type
+
+    def forward(self, x):
+        y = self.base_model(x)
+        return y.split(1, 1)
+
+    def time_derivative(self, x):
+        f1, f2 = self.forward(x)
+
+        if self.field_type != "solenoidal":
+            d_f1 = torch.autograd.grad(f1.sum(), x, create_graph=True)[0]
+            conservative_field = d_f1 @ torch.eye(*self._permute_mat.shape)
+        else:
+            conservative_field = torch.zeros_like(x)
+
+        if self.field_type != "conservative":
+            d_f2 = torch.autograd.grad(f2.sum(), x, create_graph=True)[0]
+            solenoidal_field = d_f2 @ self._permute_mat.t()
+        else:
+            solenoidal_field = torch.zeros_like(x)
+
+        return HnnTimeDerivativeFields(conservative=conservative_field,
+                                       solenoidal=solenoidal_field)
+
+
+class MLP(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, depth=3,
+                 nonlinearity=torch.nn.Tanh):
+        super().__init__()
+        assert depth >= 2
+        layers = [torch.nn.Linear(input_dim, hidden_dim), nonlinearity()]
+        for i in range(depth - 2):
+            layers.append(torch.nn.Linear(hidden_dim, hidden_dim))
+            layers.append(nonlinearity())
+        layers.append(torch.nn.Linear(hidden_dim, output_dim))
+        self.ops = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.ops(x)
