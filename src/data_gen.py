6073a060947a10608673decf033b1beee9d76de3
Karl Otness <karl.otness@nyu.edu>
2020-10-04 18:23:50 -0400

    Add data generation script for use with launcher.

---
+++ src/data_gen.py
@@ -0,0 +1,37 @@
+import logging
+import pathlib
+import numpy as np
+from systems import spring
+import json
+
+
+def run_phase(base_dir, out_dir, phase_args):
+    logger = logging.getLogger("data_gen")
+    base_dir = pathlib.Path(base_dir)
+    out_dir = pathlib.Path(out_dir)
+
+    logger.info("Starting data generation")
+    system = phase_args["system"]
+    system_args = phase_args["system_args"]
+    if system == "spring":
+        sys_result = spring.generate_data(system_args=system_args,
+                                          base_logger=logger)
+    elif system == "wave":
+        sys_result = None
+    else:
+        raise ValueError(f"Invalid system: {system}")
+
+    # Save system trajectories
+    logger.info("Saving system results")
+    results_file = out_dir / "trajectories.npz"
+    np.savez(results_file, **sys_result.trajectories)
+    logger.info(f"Saved trajectories to: {results_file}")
+
+    # Save system metadata
+    with open(out_dir / "system_meta.json", "w", encoding="utf8") as meta_file:
+        json.dump(meta_file, {"system": system,
+                              "system_args": system_args,
+                              "trajectories": sys_result.metadata})
+    logger.info("Saved system metadata")
+
+    logger.info("Data generation done")
