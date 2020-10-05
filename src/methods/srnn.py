8c4dc9b4494a43cf7167dac24c8a65a1107178c4
Karl Otness <karl.otness@nyu.edu>
2020-10-04 22:16:27 -0400

    Add network construction function for SRNN.

--- src/methods/srnn.py
+++ src/methods/srnn.py
@@ -1,5 +1,6 @@
 import torch
 from torch.autograd import grad
+from .defs import NONLINEARITIES
 
 
 class MLP(torch.nn.Module):
@@ -16,6 +17,7 @@ class MLP(torch.nn.Module):
     def forward(self, x):
         return self.ops(x)
 
+
 class SRNN(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, depth=3, nonlinearity=torch.nn.Tanh):
         super().__init__()
@@ -30,3 +32,13 @@ class SRNN(torch.nn.Module):
 
     def forward(self, p, q):
         return self.kinetic_energy(p) + self.potential_energy(q)
+
+
+def build_network(arch_args):
+    input_dim = arch_args["input_dim"]
+    hidden_dim = arch_args["hidden_dim"]
+    output_dim = arch_args["output_dim"]
+    depth = arch_args["depth"]
+    nonlinearity = NONLINEARITIES[arch_args["nonlinearity"]]
+    return SRNN(input_dim=input_dim, hidden_dim=hidden_dim,
+                output_dim=output_dim, depth=depth, nonlinearity=nonlinearity)
