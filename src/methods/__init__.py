0de2de75cd5c3328361d1ea1bb7697bc9c359296
Karl Otness <karl.otness@nyu.edu>
2020-10-04 21:25:51 -0400

    Add dispatch function to construct networks from description.

---
+++ src/methods/__init__.py
@@ -0,0 +1,11 @@
+from . import hnn, srnn
+
+def build_network(net_args):
+    arch = net_args["arch"]
+    arch_args = net_args["arch_args"]
+    if arch == "hnn":
+        return hnn.build_network(arch_args)
+    elif arch == "srnn":
+        return srnn.build_network(arch_args)
+    else:
+        raise ValueError(f"Invalid architecture: {arch}")
