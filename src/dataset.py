4cbd321e731b40489835cfd00a9c9230cbdfeab1
Karl Otness <karl.otness@nyu.edu>
2020-10-11 15:47:39 -0400

    Add dataset implementation for snapshot-based training.
    This dataset returns individual snapshots sampled from all
    trajectories.

--- src/dataset.py
+++ src/dataset.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 from torch.utils import data
 
+
 class TrajectoryDataset(data.Dataset):
     """Returns batches of full trajectories.
     dataset[idx] -> a set of snapshots for a full trajectory"""
@@ -40,6 +41,56 @@ class TrajectoryDataset(data.Dataset):
         return len(self._trajectory_meta)
 
 
+class SnapshotDataset(data.Dataset):
+    Snapshot = namedtuple("Snapshot", ["name", "p", "q", "dp_dt", "dq_dt",
+                                       "t", "trajectory_meta"])
+
+    def __init__(self, traj_dataset):
+        super().__init__()
+        self._traj_dataset = traj_dataset
+
+        self.system = self._traj_dataset.system
+        self.system_metadata = self._traj_dataset.system_metadata
+
+        name = []
+        p = []
+        q = []
+        dp_dt = []
+        dq_dt = []
+        t = []
+        traj_meta = []
+
+        for traj_i in range(len(self._traj_dataset)):
+            traj = self._traj_dataset[traj_i]
+            # Stack the components
+            traj_num_steps = traj.p.shape[0]
+            name.extend([traj.name] * traj_num_steps)
+            p.append(traj.p)
+            q.append(traj.q)
+            dp_dt.append(traj.dp_dt)
+            dq_dt.append(traj.dq_dt)
+            t.append(traj.t)
+            traj_meta.extend([traj.trajectory_meta] * traj_num_steps)
+
+        # Load each trajectory and join the components
+        self._name = name
+        self._p = np.concatenate(p)
+        self._q = np.concatenate(q)
+        self._dp_dt = np.concatenate(dp_dt)
+        self._dq_dt = np.concatenate(dq_dt)
+        self._t = np.concatenate(t)
+        self._traj_meta = traj_meta
+
+    def __getitem__(self, idx):
+        return self.Snapshot(name=self._name[idx],
+                             trajectory_meta=self._traj_meta[idx],
+                             p=self._p[idx], q=self._q[idx],
+                             dp_dt=self._dp_dt[idx], dq_dt=self._dq_dt[idx],
+                             t=self._t[idx])
+
+    def __len__(self):
+        return len(self._traj_meta)
+
+
 # TODO: Other dataset wrappers for later (wrap a TrajectoryDataset) to select:
-# - Individual samples
 # - Rollout-sized chunks
