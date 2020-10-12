a9370aff45015323f29e3a66beb21916b28b7532
Karl Otness <karl.otness@nyu.edu>
2020-10-12 13:30:19 -0400

    Fix evaluation script for SRNN runs.

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,9 +14,10 @@ import integrators
 METHOD_HNET = 5
 
 
-def load_network(net_dir, base_logger):
+def load_network(net_dir, base_dir, base_logger):
     logger = base_logger.getChild("load_network")
     # Load metadata
+    net_dir = base_dir / pathlib.Path(net_dir)
     meta_path = net_dir / "model.json"
     with open(meta_path, "r", encoding="utf8") as meta_file:
         metadata = json.load(meta_file)
@@ -33,8 +34,14 @@ def load_network(net_dir, base_logger):
 def create_dataset(base_dir, data_args):
     data_dir = base_dir / data_args["data_dir"]
     data_set = dataset.TrajectoryDataset(data_dir=data_dir)
-    loader = utils.DataLoader(data_set, batch_size=1, shuffle=False)
+    loader = utils.data.DataLoader(data_set, batch_size=1, shuffle=False)
     return data_set, loader
+
+
+def rel_err(approx, true, norm=2):
+    num = np.linalg.norm(approx - true, ord=norm, axis=1)
+    denom = np.linalg.norm(true, ord=norm, axis=1)
+    return num / denom
 
 
 def run_phase(base_dir, out_dir, phase_args):
@@ -45,10 +52,10 @@ def run_phase(base_dir, out_dir, phase_args):
 
     # Choose the device and dtype
     device = select_device(try_gpu=eval_args["try_gpu"], base_logger=logger)
-    eval_dtype = TRAIN_DTYPES[eval_args.get("train_dtype", "float")]
+    eval_dtype = TRAIN_DTYPES[eval_args.get("eval_dtype", "float")]
 
     # Load the network
-    net = load_network(phase_args["eval_net"], base_logger=logger)
+    net = load_network(phase_args["eval_net"], base_dir=base_dir, base_logger=logger)
     net = net.to(device, dtype=eval_dtype)
 
     # Load the evaluation data
@@ -59,12 +66,12 @@ def run_phase(base_dir, out_dir, phase_args):
     eval_type = eval_args["eval_type"]
     integrator_type = eval_args["integrator"]
 
-    if eval_type == "hnn":
+    if eval_type == "srnn":
         time_deriv_func = net
-    elif eval_type == "srnn":
+    elif eval_type == "hnn":
         def model_hamiltonian(p, q):
-            stacked_input = torch.stack([p, q], dim=-1)
-            return net(stacked_input, split=False)
+            stacked_input = torch.cat([p, q], dim=-1)
+            return net.time_derivative(stacked_input)
         time_deriv_func = model_hamiltonian
     else:
         logger.error(f"Invalid evaluation type: {eval_type}")
@@ -78,8 +85,8 @@ def run_phase(base_dir, out_dir, phase_args):
         q = trajectory.q.to(device, dtype=eval_dtype)
         num_time_steps = trajectory.trajectory_meta["num_time_steps"][0]
         time_step_size = trajectory.trajectory_meta["time_step_size"][0]
-        p0 = p[0].unsqueeze(0)
-        q0 = q[0].unsqueeze(0)
+        p0 = p[:, 0]
+        q0 = q[:, 0]
         integrate_start = time.perf_counter()
         int_res = integrators.numerically_integrate(
             integrator_type,
@@ -92,19 +99,39 @@ def run_phase(base_dir, out_dir, phase_args):
             volatile=True,
             device=device,
             coarsening_factor=1).permute(1, 0, 2)
+        # Remove extraneous batch dimension
+        int_res = int_res[0].detach().cpu().numpy()
         integrate_elapsed = time.perf_counter() - integrate_start
-        trajectory_results.append((traj_name, int_res, integrate_elapsed))
+
+        # Compute errors and other statistics
+        true = torch.cat([p, q], axis=-1)[0].detach().cpu().numpy()
+        rel_l2 = rel_err(approx=int_res, true=true, norm=2)
+
+        int_numpy_arrays = {
+            traj_name: int_res,
+            f"{traj_name}_relerr_l2": rel_l2,
+        }
+
+        int_stats = {
+            "name": traj_name,
+            "file_names": {
+                "traj": traj_name,
+                "relerr_l2": f"{traj_name}_relerr_l2",
+            },
+            "timing": {
+                "integrate_elapsed": integrate_elapsed
+            }
+        }
+
+        trajectory_results.append((traj_name, int_numpy_arrays, int_stats))
         logger.info(f"Integrated trajectory: {traj_num} ({traj_name}) in {integrate_elapsed} sec")
 
     # Process for save
     results_meta = {"integration_stats": []}
     saved_trajectories = {}
-    for traj_name, int_res, int_elapsed_time in trajectory_results:
-        saved_trajectories[traj_name] = int_res
-        results_meta["integration_stats"].append({
-            "name": traj_name,
-            "elapsed_time": int_elapsed_time,
-        })
+    for _traj_name, int_res, int_stats in trajectory_results:
+        saved_trajectories.update(int_res)
+        results_meta["integration_stats"].append(int_stats)
 
     # Save integration results
     int_traj_file = out_dir / "integrated_trajectories.npz"
@@ -112,7 +139,7 @@ def run_phase(base_dir, out_dir, phase_args):
     logger.info(f"Saved integrated trajectories to {int_traj_file}")
 
     # Save metadata
-    int_meta_file = out_dir / "results_meta.json",
+    int_meta_file = out_dir / "results_meta.json"
     with open(int_meta_file, "w", encoding="utf8") as meta_file:
         json.dump(results_meta, meta_file)
     logger.info(f"Saved trajectory integration metadata to {int_meta_file}")
