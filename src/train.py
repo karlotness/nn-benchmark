07515d614b067953452de5d302f00637fdf2b51d
Karl Otness <karl.otness@nyu.edu>
2020-11-08 23:45:00 -0500

    Update MLP training to use names.

--- src/train.py
+++ src/train.py
@@ -181,9 +181,9 @@ def run_phase(base_dir, out_dir, phase_args):
                     total_loss_denom += p.shape[0] * p.shape[1]
                 elif train_type == "mlp":
                     # Assume snapshot dataset (shape [batch_size, n_grid])
-                    x = torch.cat([p, q], dim=-1)
+                    deriv_pred = net(p=p, q=q)
                     dx_dt = torch.cat([dp_dt, dq_dt], dim=-1)
-                    dx_dt_pred = net(x)
+                    dx_dt_pred = torch.cat([deriv_pred.dp_dt, deriv_pred.dq_dt], dim=-1)
                     loss = loss_fn(dx_dt_pred, dx_dt)
                     total_loss_denom += p.shape[0]
                 else:
