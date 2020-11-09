ac4e15e53ae6788ea1592d86a02f98f548321b3b
Karl Otness <karl.otness@nyu.edu>
2020-11-08 21:31:07 -0500

    Update base class in defs.
    Doesn't affect anything in practice, just remove the differences.

--- src/systems/defs.py
+++ src/systems/defs.py
@@ -9,15 +9,15 @@ class System:
     def __init__(self):
         pass
 
-    def hamiltonian(self, coord):
+    def hamiltonian(self, q, p):
         """Compute hamiltonian for each coord. Input [batch, 2, n_grid] or [2, n_grid]. Return [batch] or scalar"""
         raise NotImplementedError("Subclass this")
 
-    def derivative(self, coord):
+    def derivative(self, q, p):
         """Return local q, p time derivative for each of coord. Input shape [batch, 2, n_grid] or [2, n_grid]"""
         raise NotImplementedError("Subclass this")
 
-    def generate_trajectory(self, x0, t_span, time_step_size, **kwargs):
+    def generate_trajectory(self, q0, p0, num_time_steps, time_step_size, **kwargs):
         """x0 is initial state, t_span = (t_min, t_max), time_step_size with
         t_span determines # steps
         """
