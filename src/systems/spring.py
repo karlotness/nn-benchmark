032dd0787539566214d787543f7b7345b5861f68
Karl Otness <karl.otness@nyu.edu>
2020-11-08 21:10:33 -0500

    Remove un-named, combined coordinates from spring system.

--- src/systems/spring.py
+++ src/systems/spring.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.integrate import solve_ivp
-from .defs import System, TrajectoryResult, SystemResult
+from .defs import System, TrajectoryResult, SystemResult, StatePair
 import logging
 import time
 
@@ -9,27 +9,30 @@ class SpringSystem(System):
     def __init__(self):
         super().__init__()
 
-    def hamiltonian(self, coord):
-        q, p = np.split(coord.T, 2)
+    def hamiltonian(self, q, p):
         return q**2 + p**2
 
-    def _hamiltonian_grad(self, coord):
-        return 2 * coord
+    def _hamiltonian_grad(self, q, p):
+        return StatePair(q=2*q, p=2*p)
 
     def _dynamics(self, time, coord):
-        return self.derivative(coord)
+        q, p = np.split(coord, 2)
+        deriv = self.derivative(q=q, p=p)
+        return np.stack((deriv.q, deriv.p), axis=-1)
 
-    def derivative(self, coord):
-        grads = self._hamiltonian_grad(coord)
-        dqdt, dpdt = np.split(grads, 2)
-        return np.concatenate([dpdt, -dqdt], axis=-1)
+    def derivative(self, q, p):
+        grad = self._hamiltonian_grad(q=q, p=p)
+        dqdt, dpdt = grad.q, grad.p
+        return StatePair(q=dpdt, p=-dqdt)
 
-    def generate_trajectory(self, x0, t_span, time_step_size, rtol=1e-10,
+    def generate_trajectory(self, q0, p0, t_span, time_step_size, rtol=1e-10,
                             noise_sigma=0.0):
         t_min, t_max = t_span
         assert t_min < t_max
         num_steps = np.ceil((t_max - t_min) / time_step_size)
         t_eval = np.arange(num_steps) * time_step_size + t_min
+
+        x0 = np.stack((q0, p0))
 
         spring_ivp = solve_ivp(fun=self._dynamics, t_span=t_span, y0=x0,
                                t_eval=t_eval, rtol=rtol)
@@ -68,8 +71,14 @@ def generate_data(system_args, base_logger=None):
         traj_name = f"traj_{i:05}"
         logger.info(f"Generating trajectory {traj_name}")
 
+        # Extract initial condition (either dict[q, p] or a list[q, p])
+        if isinstance(traj_def["initial_condition"], dict):
+            q0 = traj_def["initial_condition"]["q"]
+            p0 = traj_def["initial_condition"]["p"]
+        else:
+            q0 = traj_def["initial_condition"][0]
+            p0 = traj_def["initial_condition"][1]
         # Extract parameters
-        x0 = np.array(traj_def["initial_condition"], dtype=np.float64)
         num_time_steps = traj_def["num_time_steps"]
         time_step_size = traj_def["time_step_size"]
         noise_sigma = traj_def.get("noise_sigma", 0.0)
@@ -78,7 +87,8 @@ def generate_data(system_args, base_logger=None):
 
         # Generate trajectory
         traj_gen_start = time.perf_counter()
-        traj_result = system.generate_trajectory(x0=x0,
+        traj_result = system.generate_trajectory(p0=p0,
+                                                 q0=q0,
                                                  t_span=t_span,
                                                  time_step_size=time_step_size,
                                                  rtol=rtol,
