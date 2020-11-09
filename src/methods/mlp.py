561ff7ddfa41d8e4ea03a37cca389942b7e2e517
Karl Otness <karl.otness@nyu.edu>
2020-11-08 23:42:53 -0500

    Update MLP to take and return named (q, p) values.

--- src/methods/mlp.py
+++ src/methods/mlp.py
@@ -1,6 +1,8 @@
+from collections import namedtuple
 import torch
 from .defs import NONLINEARITIES
 
+TimeDerivative = namedtuple("TimeDerivative", ["dq_dt", "dp_dt"])
 
 class MLP(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, depth=3,
@@ -14,8 +16,12 @@ class MLP(torch.nn.Module):
         layers.append(torch.nn.Linear(hidden_dim, output_dim))
         self.ops = torch.nn.Sequential(*layers)
 
-    def forward(self, x):
-        return self.ops(x)
+    def forward(self, q, p):
+        x = torch.cat([p, q], dim=-1)
+        ret = self.ops(x)
+        split_size = ret.shape[-1] // 2
+        dp, dq = torch.split(ret, split_size, dim=-1)
+        return TimeDerivative(dq_dt=dq, dp_dt=dp)
 
 
 def build_network(arch_args):
