36441b143ebd2f17aeaa34a674d4e50966f34f44
Karl Otness <karl.otness@nyu.edu>
2020-11-13 01:26:53 -0500

    Add lazy import for HOGN implementation.

--- src/methods/__init__.py
+++ src/methods/__init__.py
@@ -12,5 +12,9 @@ def build_network(net_args):
         return mlp.build_network(arch_args)
     elif arch == "knn-regressor":
         return neighbors.KNeighborsRegressor(n_neighbors=1)
+    elif arch == "hogn":
+        # Lazy import HOGN to avoid pytorch-geometric if possible
+        from . import hogn
+        return hogn.build_network(arch_args)
     else:
         raise ValueError(f"Invalid architecture: {arch}")
