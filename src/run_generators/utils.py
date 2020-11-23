916d567c8d567784f78abd5ca94580b2feb3e4ca
Karl Otness <karl.otness@nyu.edu>
2020-11-23 11:47:05 -0500

    Add definition of saveable spring dataset.

--- src/run_generators/utils.py
+++ src/run_generators/utils.py
@@ -1,5 +1,7 @@
 import copy
 import numpy as np
+import pathlib
+import json
 
 
 class Experiment:
@@ -7,10 +9,15 @@ class Experiment:
         self.name = name
         self._counter = 0
 
-    def get_run_suffix(self):
+    def _get_run_suffix(self):
         count = self._counter
         self._counter += 1
         return count
+
+    def get_run_name(self, name_core):
+        suffix = self._get_run_suffix()
+        name = f"{self.name}_{name_core}_{suffix:05}"
+        return name
 
 
 class InitialConditionSource:
@@ -78,3 +85,90 @@ class SpringInitialConditionSource(InitialConditionSource):
             },
         }
         return state
+
+
+class SavableDescription:
+    def __init__(self, experiment, phase, name):
+        self.experiment = experiment
+        self.name = self.experiment.get_run_name(name)
+        self.path = f"run/{phase}/{self.name}"
+        self._descr_path = f"descr/{phase}/{self.name}.json"
+        self.phase = phase
+
+    def description(self):
+        # Subclasses provide top-level dictionary including slurm_args
+        # So dictionary with "phase_args" and "slurm_args" keys
+        # Rest is filled in here
+        raise NotImplementedError("Subclass this")
+
+    def write_description(self, base_dir):
+        base_dir = pathlib.Path(base_dir)
+        out_path = base_dir / pathlib.Path(self._descr_path)
+        descr = self.description()
+        # Update core values
+        descr.update({
+            "out_dir": self.path,
+            "phase": self.phase,
+            "exp_name": self.experiment.name,
+            "run_name": self.name,
+        })
+        # Write description to file
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(out_path, "w", encoding="utf8") as out_file:
+            json.dump(descr, out_file)
+
+
+class Dataset(SavableDescription):
+    def __init__(self, experiment, name_tail, set_type="train"):
+        super().__init__(experiment=experiment,
+                         phase="data_gen",
+                         name=f"{set_type}-{name_tail}")
+        self.set_type = set_type
+
+    def data_dir(self):
+        return self.path
+
+
+class SpringDataset(Dataset):
+    def __init__(self, experiment, initial_cond_source, num_traj,
+                 set_type="train",
+                 num_time_steps=30, time_step_size=0.3, rtol=1e-10,
+                 noise_sigma=0.0):
+        super().__init__(experiment=experiment,
+                         name_tail=f"spring-n{num_traj}-t{num_time_steps}",
+                         set_type=set_type)
+        self.num_traj = num_traj
+        self.initial_cond_source = initial_cond_source
+        self.num_time_steps = num_time_steps
+        self.time_step_size = time_step_size
+        self.rtol = rtol
+        self.noise_sigma = noise_sigma
+        self.initial_conditions = self.initial_cond_source.sample_initial_conditions(self.num_traj)
+
+    def description(self):
+        # Build trajectories
+        trajectories = []
+        for icond in self.initial_conditions:
+            traj = {
+                "num_time_steps": self.num_time_steps,
+                "time_step_size": self.time_step_size,
+                "rtol": self.rtol,
+                "noise_sigma": self.noise_sigma
+            }
+            traj.update(icond)
+            trajectories.append(traj)
+        template = {
+            "phase_args": {
+                "system": "spring",
+                "system_args": {
+                    "trajectory_defs": trajectories
+                }
+            },
+            "slurm_args": {
+                "gpu": False,
+                "time": "00:30:00",
+                "cpus": 8,
+                "mem": 6,
+            },
+        }
+        return template
