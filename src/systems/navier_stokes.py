390405ae25342126887bf7952893eaa7f411f079
Karl Otness <karl.otness@nyu.edu>
2021-04-23 19:09:29 -0400

    Add initial Navier-Stokes system using PolyFEM.

---
+++ src/systems/navier_stokes.py
@@ -0,0 +1,243 @@
+import logging
+import codecs
+import subprocess
+import json
+import os
+import pathlib
+import shutil
+import tempfile
+import lzma
+import time
+from collections import namedtuple
+from .defs import System, SystemCache, SystemResult
+import numpy as np
+
+
+IN_MESH_PATH = pathlib.Path(__file__).parent / "resources" / "mesh.obj.xz"
+navier_stokes_cache = SystemCache()
+NavierStokesTrajectoryResult = namedtuple("NavierStokesTrajectoryResult",
+                                          ["grids",
+                                           "solutions",
+                                           "grads",
+                                           "pressures",
+                                           "pressures_grads",
+                                           "t"])
+
+
+class NavierStokesSystem(System):
+    def __init__(self, grid_resolution=0.01, viscosity=0.001, base_logger=None):
+        super().__init__()
+        self.grid_resolution = grid_resolution
+        self.viscosity = viscosity
+        if base_logger:
+            self.logger = base_logger.getChild("ns-system")
+        else:
+            self.logger = logging.getLogger("ns-system")
+
+    def _args_compatible(self, grid_resolution, viscosity, base_logger=None):
+        return (self.grid_resolution == grid_resolution and
+                self.viscosity == viscosity)
+
+    def _gen_config(self, mesh_file, num_time_steps, time_step_size, in_velocity):
+        t_end = num_time_steps * time_step_size
+        return {
+            "mesh": mesh_file,
+            "normalize_mesh": False,
+            "discr_order": 2,
+            "pressure_discr_order": 1,
+            "BDF_order": 3,
+            "n_refs": 0,
+            "tend": t_end,
+            "time_steps": num_time_steps,
+            "vismesh_rel_area": 0.0001,
+            "problem_params": {
+                "U": in_velocity,
+                "time_dependent": True,
+            },
+            "params": {
+                "viscosity": self.viscosity,
+            },
+            "solver_params": {
+                "gradNorm": 1e-8,
+                "nl_iterations": 20,
+            },
+            "export": {
+                "sol_on_grid": self.grid_resolution,
+            },
+            "problem": "FlowWithObstacle",
+            "tensor_formulation": "NavierStokes",
+        }
+
+
+    def generate_trajectory(self, num_time_steps, time_step_size, in_velocity):
+        # Create temporary directory
+        with tempfile.TemporaryDirectory(prefix="polyfem-") as _tmp_dir:
+            self.logger.info(f"Generating trajectory in {_tmp_dir}")
+            tmp_dir = pathlib.Path(_tmp_dir)
+
+            # Write the mesh file
+            with open(tmp_dir / "mesh.obj", "wb") as mesh_file:
+                with lzma.open(IN_MESH_PATH, "r") as in_mesh_file:
+                    mesh_file.write(in_mesh_file.read())
+
+            # Write the config file
+            with open(tmp_dir / "config.json", "w", encoding="utf8") as config_file:
+                config = self._gen_config(
+                    mesh_file="mesh.obj",
+                    num_time_steps=num_time_steps,
+                    time_step_size=time_step_size,
+                    in_velocity=in_velocity,
+                )
+                json.dump(config, config_file)
+
+            # Run PolyFEM
+            polyfem_logger = self.logger.getChild("polyfem")
+            prog = shutil.which(str(pathlib.Path(os.environ.get("POLYFEM_BIN_DIR", "")) / "PolyFEM_bin"))
+            if not prog:
+                raise ValueError("Cannot find PolyFEM; please install and set POLYFEM_BIN_DIR")
+            cmdline = [prog, "--json", "config.json", "--cmd"]
+            new_env = dict(os.environ)
+            if "OMP_NUM_THREADS" not in new_env:
+                new_env["OMP_NUM_THREADS"] = os.environ.get("SLURM_JOB_CPUS_PER_NODE", "1")
+            self.logger.info(f"Launching PolyFEM: {cmdline}")
+            with subprocess.Popen(cmdline, stdout=subprocess.PIPE, env=new_env, cwd=tmp_dir) as proc:
+                for line in proc.stdout:
+                    level = logging.DEBUG
+                    str_line = codecs.decode(line, encoding="utf-8").strip()
+                    if "[info]" in str_line:
+                        # Crop the line before logging
+                        str_line = str_line[str_line.index("[info]") + 7:]
+                        level = logging.INFO
+                    polyfem_logger.log(level, str_line)
+            if proc.returncode != 0:
+                raise RuntimeError(f"PolyFEM failed: {proc.returncode}")
+            else:
+                self.logger.info("PolyFEM finished")
+
+            # Gather results
+            grids = []
+            solutions = []
+            grads = []
+            pressures = []
+            pressures_grads = []
+            for i in range(num_time_steps):
+                grids.append(np.loadtxt(tmp_dir / f"step_{i}.vtu_grid.txt"))
+                solutions.append(np.loadtxt(tmp_dir / f"step_{i}.vtu_sol.txt"))
+                grads.append(np.loadtxt(tmp_dir / f"step_{i}.vtu_grad.txt"))
+                pressures.append(np.loadtxt(tmp_dir / f"step_{i}.vtu_p_sol.txt"))
+                pressures_grads.append(np.loadtxt(tmp_dir / f"step_{i}.vtu_p_grad.txt"))
+            grids = np.stack(grids)
+            solutions = np.stack(solutions)
+            grads = np.stack(grads)
+            pressures = np.stack(pressures)
+            pressures_grads = np.stack(pressures_grads)
+
+        # Repackage results
+        t = time_step_size * np.arange(num_time_steps)
+        return NavierStokesTrajectoryResult(
+            grids=grids,
+            solutions=solutions,
+            grads=grads,
+            pressures=pressures,
+            pressures_grads=pressures_grads,
+            t=t,
+        )
+
+
+def system_from_records(grid_resolution, viscosity, base_logger=None):
+    cached_sys = navier_stokes_cache.find(
+        grid_resolution=grid_resolution,
+        viscosity=viscosity,
+        base_logger=logger
+    )
+    if cached_sys is not None:
+        return cached_sys
+    else:
+        new_sys = NavierStokesSystem(
+            grid_resolution=grid_resolution,
+            viscosity=viscosity,
+            base_logger=logger
+        )
+        navier_stokes_cache.insert(new_sys)
+        return new_sys
+
+
+def generate_data(system_args, base_logger=None):
+    if base_logger:
+        logger = base_logger.getChild("navier-stokes")
+    else:
+        logger = logging.getLogger("navier-stokes")
+
+    trajectory_metadata = []
+    trajectories = {}
+
+    grid_resolution = system_args.get("grid_resolution", 0.01)
+    trajectory_defs = system_args["trajectory_defs"]
+    for i, traj_def in enumerate(trajectory_defs):
+        traj_name = f"traj_{i:05}"
+        logger.info(f"Generating trajectory {traj_name}")
+
+        num_time_steps = traj_def["num_time_steps"]
+        time_step_size = traj_def["time_step_size"]
+        in_velocity = traj_def["in_velocity"]
+        viscosity = traj_def.get("viscosity", 0.001)
+
+        # Create system
+        system = navier_stokes_cache.find(grid_resolution=grid_resolution, viscosity=viscosity, base_logger=logger)
+        if system is None:
+            system = NavierStokesSystem(grid_resolution=grid_resolution, viscosity=viscosity, base_logger=logger)
+            navier_stokes_cache.insert(system)
+
+        # Generate trajectory
+        traj_gen_start = time.perf_counter()
+        traj_result = system.generate_trajectory()
+        traj_gen_elapsed = time.perf_counter() - traj_gen_start
+        logger.info(f"Generated {traj_name} in {traj_gen_elapsed} sec")
+
+        # Store trajectory data
+        trajectories.update({
+            f"{traj_name}_grids": traj_result.grids,
+            f"{traj_name}_solutions": traj_result.solutions,
+            f"{traj_name}_grads": traj_result.grads,
+            f"{traj_name}_pressures": traj_result.pressures,
+            f"{traj_name}_pressures_grads": traj_result.pressures_grads,
+            f"{traj_name}_t": traj_result.t,
+        })
+
+        # Store per-trajectory metadata
+        trajectory_metadata.append(
+            {"name": traj_name,
+             "num_time_steps": num_time_steps,
+             "time_step_size": time_step_size,
+             "in_velocity": in_velocity,
+             "viscosity": viscosity,
+             "noise_sigma": 0,
+             "field_keys": {
+                 # Plain names
+                 "grids": f"{traj_name}_grids",
+                 "solutions": f"{traj_name}_solutions",
+                 "grads": f"{traj_name}_grads",
+                 "pressures": f"{traj_name}_pressures",
+                 "pressures_grads": f"{traj_name}_pressures_grads",
+                 # Mapped names
+                 "p": f"{traj_name}_solutions",
+                 "q": f"{traj_name}_pressures",
+                 "dpdt": f"{traj_name}_grads",
+                 "dqdt": f"{traj_name}_pressures_grads",
+                 "t": f"{traj_name}_t",
+                 "p_noiseless": f"{traj_name}_solutions",
+                 "q_noiseless": f"{traj_name}_pressures",
+             },
+             "timing": {
+                 "traj_gen_time": traj_gen_elapsed
+             }})
+
+    logger.info("Done generating trajectories")
+
+    return SystemResult(
+        trajectories=trajectories,
+        metadata={
+            "grid_resolution": grid_resolution,
+            "viscosity": viscosity,
+        },
+        trajectory_metadata=trajectory_metadata)
