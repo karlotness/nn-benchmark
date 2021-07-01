a651db165c2a8690c34a096fa32063d1565e81ac
Karl Otness <karl.otness@nyu.edu>
2021-07-01 15:51:02 -0400

    Reduce redundancy in neural network construction.

--- src/methods/__init__.py
+++ src/methods/__init__.py
@@ -1,31 +1,48 @@
-from sklearn import neighbors
-from . import mlp, nn_kernel, cnn, unet
+import importlib
+
+
+def lazy_build_network(module, pass_predict_type=True, **kwargs):
+    def build_network(arch_args, predict_type):
+        extra_args = kwargs.copy()
+        if pass_predict_type:
+            extra_args.update({"predict_type": predict_type})
+        mod = importlib.import_module(f".{module}", __name__)
+        return mod.build_network(arch_args, **extra_args)
+    return build_network
+
+
+def build_knn(arch_args, predict_type):
+    neighbors = importlib.import_module("sklearn.neighbors")
+    return neighbors.KNeighborsRegressor(n_neighbors=1)
+
+
+NETWORK_TYPES = {
+    "mlp": lazy_build_network("mlp"),
+    "cnn": lazy_build_network("cnn"),
+    "unet": lazy_build_network("unet"),
+    "gn": lazy_build_network("gn", pass_predict_type=False),
+    "nn-kernel": lazy_build_network("nn_kernel"),
+    "knn": build_knn,
+}
+
+
+def get_network_type(arch):
+    if arch in {"knn-regressor", "knn-predictor",
+                "knn-regressor-oneshot", "knn-predictor-oneshot"}:
+        return "knn", None
+    for suffix in ["step", "deriv"]:
+        dash_suffix = f"-{suffix}"
+        if arch.endswith(dash_suffix):
+            return arch[:-len(dash_suffix)], suffix
+    raise ValueError(f"Invalid architecture: {arch}")
+
 
 def build_network(net_args):
     arch = net_args["arch"]
+    arch_key, predict_type = get_network_type(arch)
     arch_args = net_args["arch_args"]
-    elif arch == "mlp-deriv":
-        return mlp.build_network(arch_args, predict_type="deriv")
-    elif arch == "mlp-step":
-        return mlp.build_network(arch_args, predict_type="step")
-    elif arch == "cnn-deriv":
-        return cnn.build_network(arch_args, predict_type="deriv")
-    elif arch == "cnn-step":
-        return cnn.build_network(arch_args, predict_type="step")
-    elif arch == "unet-deriv":
-        return unet.build_network(arch_args, predict_type="deriv")
-    elif arch == "unet-step":
-        return unet.build_network(arch_args, predict_type="step")
-    elif arch in {"knn-regressor", "knn-predictor",
-                  "knn-regressor-oneshot", "knn-predictor-oneshot"}:
-        return neighbors.KNeighborsRegressor(n_neighbors=1)
-    elif arch == "gn":
-        # Lazy import GN to avoid pytorch-geometric if possible
-        from . import gn
-        return gn.build_network(arch_args)
-    elif arch == "nn-kernel-deriv":
-        return nn_kernel.build_network(arch_args, predict_type="deriv")
-    elif arch == "nn-kernel-step":
-        return nn_kernel.build_network(arch_args, predict_type="step")
-    else:
-        raise ValueError(f"Invalid architecture: {arch}")
+    try:
+        net_func = NETWORK_TYPES[arch_key]
+    except KeyError as exc:
+        raise ValueError(f"Invalid architecture: {arch}") from exc
+    return net_func(arch_args, predict_type=predict_type)
